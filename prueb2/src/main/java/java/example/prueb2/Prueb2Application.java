2e7907228db984b1a1959ac4742c6e009db39f6b
Julian150301 <147889071+Julian150301@users.noreply.github.com>
2024-02-27 16:16:58 -0500

    Add files via upload

---
+++ prueb2/src/main/java/java/example/prueb2/Prueb2Application.java
@@ -0,0 +1,13 @@
+package java.example.prueb2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prueb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prueb2Application.class, args);
+	}
+
+}
---
+++ prueb2/src/test/java/java/example/prueb2/Prueb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package java.example.prueb2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prueb2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
